73d14e652c4a2e901c00423532179f425baac0bf
MarceloGodoy <mgodoy_desenv@hotmail.com>
2015-11-25 15:31:01 -0200

    Assinatura StrongName

--- src/smartTextureMap/smartTextureMap/Intelligence/Lens/PointManager.cs
+++ src/smartTextureMap/smartTextureMap/Intelligence/Lens/PointManager.cs
@@ -6,7 +6,12 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 
-[assembly: InternalsVisibleToAttribute("smartTextureMap.Test")]
+[assembly: InternalsVisibleToAttribute("smartTextureMap.Test, PublicKey=" +
+    "00240000048000009400000006020000002400005253413100040000010001001db304b3ca3837" +
+    "1bcae06ace586292857d3a2adbfa63bb565af98b08cc7cbcb71a7ba5d4ada5fad5a812f4a99159" +
+    "908590c8765d7c870b3f371507b1575e5aed15a9bb84ea1496b83fcba52141287bc2ccdc6939f3"+
+    "5ee2f406cd2e6065fa619134d749cdac1c519d69b20024aac78ddf17c12b9f74e79d301f167795" +
+    "b7468ba8")]
 
 namespace smartTextureMap.Intelligence.Lens{
 	/// <summary>
