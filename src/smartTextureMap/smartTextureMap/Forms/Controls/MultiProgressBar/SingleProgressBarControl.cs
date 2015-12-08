ea97da8f902b820c6c1e7d1e6a561539706d4917
MarceloGodoy <mgodoy_desenv@hotmail.com>
2015-12-08 20:28:13 -0200

    Preparação para ativação dos ProgressBar

--- src/smartTextureMap/smartTextureMap/Forms/Controls/MultiProgressBar/SingleProgressBarControl.cs
+++ src/smartTextureMap/smartTextureMap/Forms/Controls/MultiProgressBar/SingleProgressBarControl.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using smartTextureMap.Intelligence;
 
 namespace smartTextureMap.Forms.Controls.MultiProgressBar
 {
     public partial class SingleProgressBarControl : UserControl
     {
+        /// <summary>
+        /// It´s the fileName
+        /// </summary>
+        public String FileName { get; set; }
+
         /// <summary>
         /// It´s the text to display
         /// </summary>
@@ -30,6 +36,27 @@ namespace smartTextureMap.Forms.Controls.MultiProgressBar
         public SingleProgressBarControl()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Runs the process
+        /// </summary>
+        public void Run()
+        {
+            this.backgroundWorker1.RunWorkerAsync(this.FileName);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            String fileName = (String)e.Argument;
+
+            SmartTextureMap smartMap = new SmartTextureMap();
+            smartMap.Generate(fileName);
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
         }
     }
 }
